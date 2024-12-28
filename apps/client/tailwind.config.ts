cb0478971e993a45e7082462bc0df6f6d8b73000
Shyaminda Senevirathna <shyamindasenevirathna@gmail.com>
2024-12-28 15:43:22 +0530

    add shadcn ui to the ui package

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -7,6 +7,7 @@ const tailwindConfig: Config = {
 		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
 		"./components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+		"../../packages/ui/components/ui/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 };
 
--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,3 +1,69 @@
+// import type { Config } from "tailwindcss";
+// import tailwindcssAnimate from "tailwindcss-animate";
+
+// export default {
+// 	darkMode: ["class"],
+// 	content: [
+// 		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+// 		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+// 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+// 		"../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}",
+// 	],
+// 	theme: {
+// 		extend: {
+// 			colors: {
+// 				brand: {
+// 					"100": "#6c4afe",
+// 					DEFAULT: "#a48cfb",
+// 				},
+// 				red: "#FF7474",
+// 				error: "#b80000",
+// 				green: "#3DD9B3",
+// 				blue: "#56B8FF",
+// 				pink: "#EEA8FD",
+// 				orange: "#F9AB72",
+// 				light: {
+// 					"100": "#333F4E",
+// 					"200": "#A3B2C7",
+// 					"300": "#F2F5F9",
+// 					"400": "#F2F4F8",
+// 				},
+// 				dark: {
+// 					"100": "#04050C",
+// 					"200": "#131524",
+// 				},
+// 			},
+// 			fontFamily: {
+// 				poppins: ["var(--font-poppins)"],
+// 			},
+// 			boxShadow: {
+// 				"drop-1": "0px 10px 30px 0px rgba(66, 71, 97, 0.1)",
+// 				"drop-2": "0 8px 30px 0 rgba(65, 89, 214, 0.3)",
+// 				"drop-3": "0 8px 30px 0 rgba(65, 89, 214, 0.1)",
+// 			},
+// 			borderRadius: {
+// 				lg: "var(--radius)",
+// 				md: "calc(var(--radius) - 2px)",
+// 				sm: "calc(var(--radius) - 4px)",
+// 			},
+// 			keyframes: {
+// 				"caret-blink": {
+// 					"0%,70%,100%": {
+// 						opacity: "1",
+// 					},
+// 					"20%,50%": {
+// 						opacity: "0",
+// 					},
+// 				},
+// 			},
+// 			animation: {
+// 				"caret-blink": "caret-blink 1.25s ease-out infinite",
+// 			},
+// 		},
+// 	},
+// 	plugins: [tailwindcssAnimate],
+// } satisfies Config;
+
 import type { Config } from "tailwindcss";
 import tailwindcssAnimate from "tailwindcss-animate";
 
@@ -8,6 +74,7 @@ export default {
 		"./components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
 		"../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}",
+		"../../packages/ui/components/ui/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
 		extend: {
@@ -32,6 +99,39 @@ export default {
 					"100": "#04050C",
 					"200": "#131524",
 				},
+				border: "hsl(var(--border))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+				primary: {
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))",
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))",
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))",
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))",
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))",
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))",
+				},
+				card: {
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))",
+				},
 			},
 			fontFamily: {
 				poppins: ["var(--font-poppins)"],
@@ -48,12 +148,8 @@ export default {
 			},
 			keyframes: {
 				"caret-blink": {
-					"0%,70%,100%": {
-						opacity: "1",
-					},
-					"20%,50%": {
-						opacity: "0",
-					},
+					"0%,70%,100%": { opacity: "1" },
+					"20%,50%": { opacity: "0" },
 				},
 			},
 			animation: {
