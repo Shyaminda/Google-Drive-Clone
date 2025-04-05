78276f27d90d0837cee493c39963ba94178883f7
Shyaminda Senevirathna <shyamindasenevirathna@gmail.com>
2025-04-05 20:56:58 +0530

    add deployment workflow

--- apps/client/lib/mail.ts
+++ apps/client/lib/mail.ts
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-	const confirmLink = `${process.env.NEXTAUTH_URL}/auth/new-verification?token=${token}`;
+	const confirmLink = `${process.env.AUTH_URL}/auth/new-verification?token=${token}`;
 
 	await resend.emails.send({
 		from: "DriveWay@elebot.pro",
@@ -14,7 +14,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 };
 
 export const sendResetPasswordEmail = async (email: string, token: string) => {
-	const resetLink = `${process.env.NEXTAUTH_URL}/auth/new-password?token=${token}`;
+	const resetLink = `${process.env.AUTH_URL}/auth/new-password?token=${token}`;
 
 	await resend.emails.send({
 		from: "DriveWay@elebot.pro",
